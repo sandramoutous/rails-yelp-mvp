ad459866a60f6276f15f916a0aa4194709e94c6f
sandra moutoussamy <sandra.moutous@gmail.com>
2019-11-20 16:58:38 +0100

    controller, models, viewsok

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, Belgium E2 7JE',
+    phone_number: '02 03 04 05 06',
+    category: 'belgian'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '65 98 32 65 45',
+    category: 'italian'
+  },
+  {
+    name: 'Best sushi',
+    address: '45 Japan St, Japan',
+    phone_number: '25 98 48 85 45',
+    category: 'japanese'
+  },
+  {
+    name: 'La joncque',
+    address: '3 lionel jin st, China',
+    phone_number: '78 41 52 96 56',
+    category: 'chinese'
+  },
+  {
+    name: 'La caravelle',
+    address: '6 rue du vieux port, marseille',
+    phone_number: '06 85 74 96 12',
+    category: 'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
